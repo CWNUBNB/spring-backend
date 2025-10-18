c5b6e1236d3c8cd901980073ed38a793f2cc9e00
SongBeom00 <kjb001023@naver.com>
2025-10-18 12:12:41 +0900

    :sparkles: User 세팅

--- src/main/java/com/cwnu/domain/user/repository/UserRepository.java
+++ src/main/java/com/cwnu/domain/user/repository/UserRepository.java
@@ -9,9 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // ID로 유저 조회
-    Optional<User> findUserById(Long id);
-
     // 로그인 ID로 유저 존재 여부 확인
     Boolean existsByLoginId(String loginId);
 
--- src/main/java/com/cwnu/domain/user/service/UserService.java
+++ src/main/java/com/cwnu/domain/user/service/UserService.java
@@ -2,15 +2,21 @@ package com.cwnu.domain.user.service;
 
 import com.cwnu.domain.user.dto.UserSingUpRequestDto;
 import com.cwnu.domain.user.dto.UserUpdateRequestDto;
+import com.cwnu.domain.user.entity.QUser;
 import com.cwnu.domain.user.entity.User;
 import com.cwnu.domain.user.repository.UserRepository;
 import com.cwnu.global.exception.ApiExceptionCode;
 import com.cwnu.global.exception.custom.CustomRuntimeException;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.cwnu.domain.user.entity.QUser.*;
+
 
 @Service
 @RequiredArgsConstructor
@@ -41,14 +47,15 @@ public class UserService {
             throw new CustomRuntimeException(ApiExceptionCode.INVALID_REQUEST);
         }
 
-        User user = User.builder()
+        User user1 = User.builder()
                 .loginId(userRequestDto.getLoginId())
                 .password(passwordEncoder.encode(userRequestDto.getPassword()))
                 .nickname(userRequestDto.getNickname())
                 .email(userRequestDto.getEmail())
                 .build();
 
-        return userRepository.save(user).getId();
+
+        return userRepository.save(user1).getId();
     }
 
     /**
@@ -63,7 +70,7 @@ public class UserService {
         String sessionLoginId = SecurityContextHolder.getContext().getAuthentication().getName();
 
         // 2. 수정하려는 유저 정보 조회
-        User user = userRepository.findUserById(id)
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new CustomRuntimeException(ApiExceptionCode.USER_NOT_FOUND));
 
         // 3. 현재 인증된 사용자와 수정하려는 유저가 같은지 확인
